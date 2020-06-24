381cc11fee2afe7d338ad183b3cfa767d22d8212
Sharon_9707 <66082086+xl0520@users.noreply.github.com>
2020-06-23 21:52:18 -0700

    Update second_try.py

--- second_try.py
+++ second_try.py
@@ -35,7 +35,7 @@ def upload_doc_string_to_mysql(title = None, Path_ = None, Parent_landing_page_p
             insert into test values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
         val = (title, Path_, Parent_landing_page_path, Landing_page_path, Repository_path, Github_repository_path, Short_description, Long_description, Long_description_html, technical_description, databases_used, technical_databases_used, created_by, maintained_by, maintained_by_backup)
-        con.execute(sql_upload)
+        con.execute(sql_upload, val)
 
 
 upload_doc_string_to_mysql(title = follow_app, Path_ = None, Parent_landing_page_path = None, Landing_page_path = None, Repository_path = None, Github_repository_path = https://github.com/dexcom-inc/Customer-Advocacy-Searches/blob/master/Library/Complaints/follow_app.py, Short_description = Testing, Long_description = None, Long_description_html = None, technical_description = None, databases_used = customer_advocacy_pms, technical_databases_used = None, created_by = Thomas McGinn, maintained_by = Thomas McGinn, maintained_by_backup = None)
